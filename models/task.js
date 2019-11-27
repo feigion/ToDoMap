9381eb4e51605d5e6eefb2e9b351ce643163731b
hfemling <hfemling@pdx.edu>
2019-11-26 16:58:37 -0800

    Adding database connection; saving tasks that are added to the database; changing action for task form and name of location so information can be accessed.

---
+++ models/task.js
@@ -0,0 +1,15 @@
+// Modified from example here: 
+// https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/mongoose
+
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var TaskSchema = new Schema(
+    {
+        name: {type: String, required: true},
+        location: {type: String, required: true}
+    }
+);
+
+module.exports = mongoose.model('Task', TaskSchema);
--- routes/newItem.js
+++ routes/newItem.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var Task = require('../models/task');
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
+  var task = new Task(
+        { 
+            name: req.body.toDoItem,
+            location: req.body.toDoLocation 
+        }
+    )
+  task.save(function (err) {
+    // if (err) { return next(err); }
+      // res.redirect('index.html');
+  })
+
   //console.log(req.body);
   res.send('Got a new item to do...');
 });
